7cb05412aad90f0e53d065a357e9f0e8a524d3bc
olti <95oltuchpatryk@gmail.com>
2024-03-02 18:30:00 +0100

    Created basic struture of login page. Added form and controlled html elements.

--- src/app/loginPage.tsx
+++ src/app/loginPage.tsx
@@ -1,7 +1,36 @@
+"use client"
+
+import { useState } from "react";
+
 export default function LoginPage() {
+
+    const [ login, setLogin ] = useState<string>('');
+    const [ password, setPassword ] = useState<string>('');
+
+    const handleChangeLogin: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        setLogin(event.target.value);
+    }
+
+    const hangleChangePassword: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault();
+        console.log(`credentials: ${login}  ${password}`);
+        setLogin('');
+        setPassword('');
+    }
+
     return (
       <main>
-        Hello !!
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={login} onChange={handleChangeLogin} />
+                <input type="password" value={password} onChange={hangleChangePassword} />
+                <button type="submit">login</button>
+            </form>
+        </div>
       </main>
     );
   }
