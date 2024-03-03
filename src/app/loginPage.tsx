b0a94112f4d7f10b94c26764b93d8d440daecc06
Patryk Oltuch <95oltuchpatryk@gmail.com>
2024-03-03 17:26:00 +0100

    Styled login page.

--- src/app/loginPage.tsx
+++ src/app/loginPage.tsx
@@ -24,11 +24,12 @@ export default function LoginPage() {
 
     return (
       <main>
-        <div className="text-blue">
-            <form onSubmit={handleSubmit}>
-                <input type="text" value={login} onChange={handleChangeLogin} />
-                <input type="password" value={password} onChange={hangleChangePassword} />
-                <button type="submit">login</button>
+        <div className="w-1/3 p-10 border-2 border-black">
+            <form className="flex flex-col justify-center items-center align-middle" onSubmit={handleSubmit}>
+                <input className="w-3/4 m-2 border-2 border-black text-center" type="text" value={login} onChange={handleChangeLogin} placeholder="login" />
+                <input className="w-3/4 m-2 border-2 border-black text-center" type="password" value={password} onChange={hangleChangePassword} placeholder="password" />
+                <button className="w-1/2 m-2 border-2 border-black" type="submit">login</button>
+                <button className="w-1/2 m-2 text-xs">reset password</button>
             </form>
         </div>
       </main>
