e560ae6db7d49d9a60d45a23ed66f73ed29bbfc7
Patryk Oltuch <95oltuchpatryk@gmail.com>
2024-03-11 08:38:54 +0100

    Added styles.

--- src/components/chat/messageBox.tsx
+++ src/components/chat/messageBox.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 import SingleMessage from "./singleMessage";
 
+import "../../app/globals.css";
+
 const MessageBox = () => {
 
     const [ message, setMessage ] = useState<string>('');
@@ -16,22 +18,25 @@ const MessageBox = () => {
     }
 
     return (
-        <div>
-            <div>
+        <div className="h-lvh border-2 border-indigo-500 w-1/2 flex-1 text-center">
+            chat.JS
+            <div className="bg-slate-100 border-2 border-black">
                 <div> mess </div>
                 <div> message </div>
             </div>
-            <div>
+            <div> 
                 <input 
                     type="text" 
                     value={message} 
                     onChange={handleSetMessage}
+                    className="border-2 border-black"
                 />
             </div>
             <div>
                 <button 
                     type="button"
                     onClick={handleSubmit}
+                    className="border-2 border-black px-4 py-1 cursor-pointer"
                 >send</button>
             </div>
         </div>
