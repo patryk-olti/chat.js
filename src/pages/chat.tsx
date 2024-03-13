9eea69f62d26e436cfa8de6f72ac162a86a5cafa
Patryk Oltuch <95oltuchpatryk@gmail.com>
2024-03-13 15:19:10 +0100

    Prepared git structure of chat page.

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -3,15 +3,15 @@ import "../app/globals.css";
 const Chat = () => {
 
     return(
-        <div>
-            <div>
+        <div className="h-screen flex flex-col">
+            <div className="border border-black">
                  menu 
             </div>
-            <div>
-                <div>
+            <div className="flex grow">
+                <div className="grow border border-red-500">
                     chat.js
                 </div>
-                <div>
+                <div className="border border-green-500">
                     user list
                 </div>
             </div>
