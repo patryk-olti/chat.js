c1d63b9b5b96fdb0bb7e4d9ee6862d2cfafa14e4
Patryk Oltuch <95oltuchpatryk@gmail.com>
2024-03-14 19:29:13 +0100

    Created model, 	controllers, 	api - for connections. Tested via postman.

---
+++ src/controllers/connection/index.js
@@ -0,0 +1,25 @@
+import Connection from '../../models/connection';
+
+export async function getConnection(req, res){
+    try {
+        const result = await Connection.find()
+        res.status(200).json({ success: true, data: result })
+    }catch (err) {
+        res.status(400).json({ success: false})
+    }
+}
+
+export async function createConnection(req, res){
+    try{
+        const { idUser, idChatroom } = req.body;
+
+        const connection = await Connection.create({
+            idUser,
+            idChatroom
+        })
+
+        return res.status(201).json({ success: true, data: connection })
+    }catch(err){
+        return res.status(400).json({ success: false })
+    }
+}
---
+++ src/pages/api/connection/index.js
@@ -0,0 +1,28 @@
+import { getConnection, createConnection } from "../../../controllers/connection";
+import dbConnect from "../../../lib/mongo";
+
+const handler = async (req, res) => {
+
+    try {
+        await dbConnect();
+    }catch(err){
+        return res.status(503).send('Database connection is not established');
+    }
+
+    const { method } = req;
+
+    switch (method){
+        case 'GET':
+            await getConnection(req, res);
+        break;
+        case 'POST':
+            await createConnection(req, res);
+        break;
+        default:
+            res.setHeader('Allow', ["GET", "POST"]);
+            res.status(425).end('Method not allowed');
+        break;
+    } 
+}
+
+export default handler
