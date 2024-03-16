a3584d02cd670ada7fe09e9d7b614d863c484269
Patryk Oltuch <95oltuchpatryk@gmail.com>
2024-03-16 16:51:43 +0100

    Created encrypt function for jwt. Created middleware for updating cookies. Created update session function.

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,7 +1,8 @@
 'use server'
 
-import { SignJWT } from 'jose';
+import { SignJWT, jwtVerify } from 'jose';
 import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
 
 const secretKey = process.env.SECRET_KEY
 const key = new TextEncoder().encode(secretKey);
@@ -12,6 +13,13 @@ export async function encrypt(payload: any){
     .setIssuedAt()
     .setExpirationTime('10 sec from now')
     .sign(key);
+}
+
+export async function decrypt(input: string):Promise<any> {
+    const { payload } = await jwtVerify(input, key, {
+        algorithms: ['HS256']
+    });
+    return payload;
 }
 
 export async function loginFunc(login: string){
@@ -27,4 +35,22 @@ export async function loginFunc(login: string){
 
     // Save the session in a cookie
     cookies().set('session', session, { expires, httpOnly: true })
+}
+
+export async function updateSession( request: NextRequest ){
+    const session = request.cookies.get('session')?.value;
+    if (!session) return;
+
+    const parsed = await decrypt(session);
+    parsed.expires = new Date(Date.now() + 10 * 1000);
+    const res = NextResponse.next();
+
+    res.cookies.set({
+        name: 'session',
+        value: await encrypt(parsed),
+        httpOnly: true,
+        expires: parsed.expires
+    });
+    
+    return res;
 }
