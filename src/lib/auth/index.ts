8d076fdb354fb3f330feda0c537442f7ed6dd271
Patryk Oltuch <95oltuchpatryk@gmail.com>
2024-03-18 08:33:07 +0100

    Fixed problem with middleware.

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -7,16 +7,17 @@ import { NextRequest, NextResponse } from 'next/server';
 
 const secretKey = process.env.SECRET_KEY
 const key = new TextEncoder().encode(secretKey);
+const secondsOfSession = 30;
 
 export async function encrypt(payload: any){
     return await new SignJWT(payload)
     .setProtectedHeader({ alg: 'HS256' })
     .setIssuedAt()
-    .setExpirationTime('10 sec from now')
+    .setExpirationTime('30 sec from now')
     .sign(key);
 }
 
-export async function decrypt(input: string):Promise<any> {
+export async function decrypt(input: string): Promise<any> {
     const { payload } = await jwtVerify(input, key, {
         algorithms: ['HS256']
     });
@@ -41,7 +42,7 @@ export async function loginFunc(login: string, password: string){
             };
 
             // Create the session
-            const expires = new Date(Date.now() + 30 * 1000);
+            const expires = new Date(Date.now() + secondsOfSession * 1000);
             const session = await encrypt({ user, expires });
 
             // Save the session in a cookie
@@ -55,6 +56,10 @@ export async function loginFunc(login: string, password: string){
     }
 }
 
+export async function logout() {
+    cookies().set('session', '', { expires: new Date(0) });
+}
+
 export async function getSession(){
     const session = cookies().get('session')?.value;
     if (!session) return null;
@@ -66,15 +71,13 @@ export async function updateSession( request: NextRequest ){
     if (!session) return;
 
     const parsed = await decrypt(session);
-    parsed.expires = new Date(Date.now() + 30 * 1000);
+    parsed.expires = new Date(Date.now() + secondsOfSession * 1000);
     const res = NextResponse.next();
-
     res.cookies.set({
         name: 'session',
         value: await encrypt(parsed),
         httpOnly: true,
         expires: parsed.expires
     });
-
     return res;
 }
