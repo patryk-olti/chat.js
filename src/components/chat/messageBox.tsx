4d8b5ada069b73edac3511bf8b178a7870a35873
Patryk Oltuch <95oltuchpatryk@gmail.com>
2024-03-20 08:34:43 +0100

    Installed react icons module and used setState for controll state of this button (only as visual).

--- src/components/chat/messageBox.tsx
+++ src/components/chat/messageBox.tsx
@@ -4,6 +4,9 @@ import SingleMessage from "./singleMessage";
 
 import "../../app/globals.css";
 
+import { IoIosSend } from "react-icons/io";
+import { AiFillLike } from "react-icons/ai";
+
 type MessageArray = {
     id: number,
     user: string,
@@ -13,6 +16,7 @@ type MessageArray = {
 
 const MessageBox = () => {
 
+    const [sendLike, setSendLike ] = useState<boolean>(true);
     const [ message, setMessage ] = useState<string>('');
     const [ messageArray, setMessageArray ] = useState<MessageArray[]>([
         {
@@ -36,6 +40,11 @@ const MessageBox = () => {
     ]);
 
     const handleSetMessage: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        if(event.target.value.length > 0){
+            setSendLike(false);
+        }else{
+            setSendLike(true);
+        }
         setMessage(event.target.value);
     }
 
@@ -90,7 +99,7 @@ const MessageBox = () => {
                         type="text" 
                         value={message} 
                         onChange={handleSetMessage}
-                        className="grow rounded-full p-2 bg-sky-200 text-black outline-none"
+                        className="grow rounded-full p-4 bg-sky-200 text-black outline-none"
                         placeholder="Aa"
                     />
                 </div>
@@ -99,7 +108,9 @@ const MessageBox = () => {
                         type="button"
                         onClick={handleSubmit}
                         className=" px-4 py-1 cursor-pointer m-2"
-                    >S</button>
+                    >
+                        { sendLike ? <AiFillLike size={30} /> : <IoIosSend size={30}/> }
+                    </button>
                 </div>
             </div>
             
