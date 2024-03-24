ac5d5677f238dd9d76536996ad83dd03e8acd70d
Patryk Oltuch <95oltuchpatryk@gmail.com>
2024-03-24 19:14:12 +0100

    Changed styles in single User component.

--- src/components/userList/singleUser.tsx
+++ src/components/userList/singleUser.tsx
@@ -11,7 +11,7 @@ const SingleUser = (props: Props) => {
     const { name } = props;
 
     return(
-        <div className="flex items-center p-2 my-1 rounded-lg cursor-pointer hover:bg-sky-200">
+        <div className="flex items-center p-1 my-1 rounded-lg cursor-pointer hover:bg-sky-200">
             <div className="shrink-0 border border-black rounded-full w-7 h-7 text-lg flex items-center justify-center bg-slate-400 mx-2"> {name.charAt(0)} </div>
             <div className="whitespace-nowrap text-lg"> {name} </div>
         </div>
