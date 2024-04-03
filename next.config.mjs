345ad4743fd95872dca5764d90c6962b91373154
Patryk Oltuch <95oltuchpatryk@gmail.com>
2024-04-03 19:53:09 +0200

    Removed code from next.config - conflicts with current project.

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,3 @@
 const nextConfig = {};
 
 export default nextConfig;
-    
-const webpack = require('webpack');
-require('dotenv').config();
-    
-module.exports = {
-    webpack: config => {
-        const env = Object.keys(process.env).reduce((acc, curr) => {
-            acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
-            return acc;
-        }, {});
-        
-        config.plugins.push(new webpack.DefinePlugin(env));
-        
-        return config;
-    }
-};
